0a9fc495c1dee2d344169b916b0a60186d5f1319
rodriguestg <grs.ibr@gmail.com>
2022-04-20 02:21:58 -0300

    subindo exercicios bônus 4.2

--- fundamentos/bloco4-introducao-javascript-logica-programacao/dia2-javascript-array-loop-for/scripts.js
+++ fundamentos/bloco4-introducao-javascript-logica-programacao/dia2-javascript-array-loop-for/scripts.js
@@ -69,7 +69,6 @@ for(let i = 0; i < limitador; i += 1) {
 }
 console.log(de1a25)
 
-console.log("")
 // EXERCICIO 9
 let numeroDividido = 0;
 let dividido2 = [];
@@ -78,3 +77,43 @@ for(let i = 0; i < de1a25.length; i += 1) {
     dividido2.push(numeroDividido);
 }
 console.log(dividido2)
+
+// BÔNUS 3
+let numbersX = [];
+let numeroMultiplicado = 0;
+for(let i = 0; i < numbers.length; i += 1) {
+  let multiplicador = numbers[i + 1];
+  let multiplicador2 = 2;
+  let numeroMultiplicado = numbers[i] * multiplicador;
+  let numeroFinal = numbers[i] * multiplicador2;
+  if ((i + 1) === numbers.length) {
+    numbersX.push(numeroFinal);
+  } else {
+    numbersX.push(numeroMultiplicado);
+  }
+}
+console.log(numbersX)
+
+// BÔNUS 1
+for (let i = 1; i < numbers.length; i += 1) {
+    for (let i2 = 0; i2 < i; i2 += 1) {
+      if(numbers[i] > numbers[i2]){
+        let position = numbers[i];
+        numbers[i] = numbers[i2];
+        numbers[i2] = position;
+    }
+  }
+}
+console.log(numbers)
+
+// BÔNUS 2
+for (let i = 1; i < numbers.length; i += 1) {
+  for (let i2 = 0; i2 < i; i2 += 1) {
+    if(numbers[i] < numbers[i2]){
+      let position = numbers[i];
+      numbers[i] = numbers[i2];
+      numbers[i2] = position;
+  }
+}
+}
+console.log(numbers)
